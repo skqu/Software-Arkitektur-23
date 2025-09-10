d23834a1d1faaa651d3b27bbb9f4733b360c2ee5
skqu <skqu@iba.dk>
2025-09-10 12:37:34 +0200

    cleanup

--- doc/html/class_sample_class.js
+++
@@ -1,5 +0,0 @@
-var class_sample_class =
-[
-    [ "getValue", "class_sample_class.html#ac25a42a76ac305a38ea5273bee8dca67", null ],
-    [ "PrintString", "class_sample_class.html#ae8a3b36e30f35ef11c49df139975a54e", null ]
-];
--- doc/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['context_0',['Context',['../md__r_e_a_d_m_e.html#autotoc_md2',1,'']]]
-];
