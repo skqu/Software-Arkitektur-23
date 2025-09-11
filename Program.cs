01cb3bd37e3041b426ff11fa6fd113994b342815
skqu <skqu@iba.dk>
2025-09-11 21:52:38 +0200

    Fixed small issues

--- Program.cs
+++ Program.cs
@@ -1,32 +1,49 @@
-﻿using AnotherNamespace;
-
-/// <summary>
-/// <para>Author: skqu </para> 
-/// <para>Date: 09-09-2025 </para>
-/// <para>Class Name: Program </para>
-/// <para>Description: This program demonstrates XML documentation comments in C#. </para>
-/// </summary>
-class Program
+﻿using System.Xml.Serialization;
+using AnotherNamespace;
+namespace Comments
 {
     /// <summary>
-    /// <para>Author: skqu </para>
+    /// <para>Author: skqu </para> 
     /// <para>Date: 09-09-2025 </para>
-    /// <para>Method name: Main </para>
-    /// <para>Description: The main entry point of the program. </para>
+    /// <para>Class Name: Program </para>
+    /// <para>Description: This program demonstrates XML documentation comments in C#. </para>
     /// </summary>
-    /// <param name="args">Command line arguments.</param>
-    /// <returns>void</returns>
-    /// <exception >None</exception>
-    static void Main(string[] args)
+    class Program
     {
-        AnotherClass anotherClass = new AnotherClass();
-        SampleClass sample = new SampleClass();
-        sample.PrintString("Hello, World!");
+        /// <summary>
+        /// <para>Author: skqu </para>
+        /// <para>Date: 11-09-2025 </para>
+        /// <para>Method name: Program </para>
+        /// <para>Description: Dummy constructor </para>
+        /// </summary>
+        /// <param>None</param>
+        /// <returns>void</returns>
+        /// <exception >None</exception>
+        protected Program()
+        {
+            
+        }
 
-        // Show comments when looking for method info
-        Console.WriteLine(sample.getValue());
+        /// <summary>
+        /// <para>Author: skqu </para>
+        /// <para>Date: 09-09-2025 </para>
+        /// <para>Method name: Main </para>
+        /// <para>Description: The main entry point of the program. </para>
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        /// <returns>void</returns>
+        /// <exception >None</exception>
+        static void Main(string[] args)
+        {
+            AnotherClass anotherClass = new AnotherClass();
+            SampleClass sample = new SampleClass();
+            sample.PrintString("Hello, World!");
 
-        Console.WriteLine(anotherClass.IsAlive());
+            // Show comments when looking for method info
+            Console.WriteLine(sample.getValue());
 
+            Console.WriteLine(anotherClass.IsAlive());
+
+        }
     }
 }
