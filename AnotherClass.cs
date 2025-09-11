1cf5162bf4145852fad38d972ae037179acc0f3a
skqu <skqu@iba.dk>
2025-09-11 21:38:08 +0200

    Solve SoncloudIssue with namespace

--- AnotherClass.cs
+++ AnotherClass.cs
@@ -1,32 +1,34 @@
-
-/// <summary>
-///  <para>Author: skqu </para>
-///  <para>Date: 10-09-2025 </para>
-/// <para>Class Name: AnotherClass </para>
-/// <para>Description: This is another sample class to demonstrate XML documentation comments. </para>
-/// </summary>
-class AnotherClass
+namespace AnotherNamespace
 {
     /// <summary>
-    /// <para>Author: skqu </para>
-    /// <para>Date: 10-09-2025 </para>
-    /// <para>Method name: IsAlive </para>
-    /// <para>Description: This method checks if the object is alive. </para>
+    ///  <para>Author: skqu </para>
+    ///  <para>Date: 10-09-2025 </para>
+    /// <para>Class Name: AnotherClass </para>
+    /// <para>Description: This is another sample class to demonstrate XML documentation comments. </para>
     /// </summary>
-    /// <returns name="aliveVar">bool: Return wether or not class is alive</returns>
-    public bool IsAlive()
+    class AnotherClass
     {
-        bool aliveVar = true;
-
-        if (aliveVar)
+        /// <summary>
+        /// <para>Author: skqu </para>
+        /// <para>Date: 10-09-2025 </para>
+        /// <para>Method name: IsAlive </para>
+        /// <para>Description: This method checks if the object is alive. </para>
+        /// </summary>
+        /// <returns name="aliveVar">bool: Return wether or not class is alive</returns>
+        public bool IsAlive()
         {
-            aliveVar = false;
-        }
-        else
-        {
-            aliveVar = true;
-        }
+            bool aliveVar = true;
 
-        return aliveVar;
+            if (aliveVar)
+            {
+                aliveVar = false;
+            }
+            else
+            {
+                aliveVar = true;
+            }
+
+            return aliveVar;
+        }
     }
 }
